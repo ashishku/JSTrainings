1f17e35b29eb7feb617cbb371f99d8e8962ebc88
Ashish Kumar <ashishku@cybage.com>
2014-07-11 12:40:02 +0530

    first commit (part 2 :()

---
+++ app/js/app.js
@@ -0,0 +1,37 @@
+/**
+ * Created by ashish on 30/6/14.
+ */
+
+var jsTraining = (function (angular) {
+  "use strict";
+
+  var jsTraining = angular.module("jsTraining", ["ngAnimate", "ngRoute", "ngResource", "angular-gestures"]);
+  jsTraining.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+      .when("/index", {
+              templateUrl:  "app/partials/indexSlide.html",
+              controller:   "IndexCtrl",
+              controllerAs: "ctrl"
+            })
+      .when("/training/:trnId", {
+              templateUrl:  "app/partials/trainigIndexSlide.html",
+              controller:   "TrainingCtrl",
+              controllerAs: "ctrl"
+            })
+      .when("/training/:trnId/agenda", {
+              templateUrl:  "app/partials/agendaSlide.html",
+              controller:   "TemplateCtrl",
+              controllerAs: "ctrl"
+            })
+      .when("/training/:trnId/template/:sldId", {
+              templateUrl:  "app/partials/templateSlide.html",
+              controller:   "TemplateCtrl",
+              controllerAs: "ctrl"
+            })
+      .otherwise({
+                   redirectTo: "/index"
+                 });
+  }]);
+
+  return jsTraining;
+})(angular);
